20d5f8c3aaf2cb7499c33a4c89f5d735deab577a
Nick Smith <nick.smith@cern.ch>
2015-09-03 18:17:22 +0200

    Event count sed

--- prefire/submit.sh
+++ prefire/submit.sh
@@ -13,3 +13,7 @@ farmoutAnalysisJobs PrefireStudy \
   --input-files-per-job=10 \
   runPrefire.py \
     'inputFiles=$inputFileNames' 'outputFile=$outputFileName' 
+
+
+# hadd -f ~/www/JetHT_run254833.root /hdfs/store/user/ncsmith/PrefireStudy-runPrefire/*.root
+# find /data/ncsmith/PrefireStudy-runPrefire/ -name *.err -size +0 -exec grep -l 'gsiftp' '{}' \; |xargs sed -n 's:.*Events total = \([0-9]*\) passed.*:\1:p' |awk '{s+=$1} END {print s}'
