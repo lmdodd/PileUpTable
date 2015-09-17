c57641aecbb203ddd75ff27e0c6b16f5efef53c3
Nick Smith <nick.smith@cern.ch>
2015-09-17 18:56:55 +0200

    Run 2015D express

---
+++ test/makePUMtableRun2015D.py
@@ -0,0 +1,75 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing('analysis')
+options.parseArguments()
+
+process = cms.Process("PUMCalc")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet( reportEvery = cms.untracked.int32(10000) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '74X_dataRun2_Prompt_v1')
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource', 'GlobalTag')
+
+# ExpressPhysics is more than just ZeroBias
+process.triggerSelection = cms.EDFilter( "TriggerResultsFilter",
+    triggerConditions = cms.vstring( 'HLT_ZeroBias_v2' ),
+    hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+    l1tResults = cms.InputTag( "gtDigis" ),
+    l1tIgnoreMask = cms.bool( False ),
+    l1techIgnorePrescales = cms.bool( False ),
+    daqPartitions = cms.uint32( 1 ),
+    throw = cms.bool( True )
+)
+
+process.load('EventFilter.RctRawToDigi.l1RctHwDigis_cfi')
+
+process.PUMcalcCentralBX = cms.EDAnalyzer("PUMcalc",
+    regionSource = cms.InputTag("rctDigis"),
+    bunchCrossingsToUse = cms.vint32(0)
+)
+
+process.PUMcalcSideBX = cms.EDAnalyzer("PUMcalc",
+    regionSource = cms.InputTag("rctDigis"),
+    bunchCrossingsToUse = cms.vint32(-2,2)
+)
+
+process.PUMtables = cms.Sequence( process.PUMcalcCentralBX + process.PUMcalcSideBX )
+
+process.PUMcorrelation = cms.EDAnalyzer("PUMcorrelation",
+    regionSource = cms.InputTag("rctDigis"),
+    lumiScalerSource = cms.InputTag("scalersRawToDigi"),
+    vertexSource = cms.InputTag("offlinePrimaryVertices"),
+    vertexCut = cms.string("ndof > 4 && abs(z) <= 50. && abs(position.rho) <= 2"),
+    bunchCrossingsToUse = cms.vint32(0)
+)
+
+process.p = cms.Path(
+        process.triggerSelection
+        *process.rctDigis
+        *process.PUMtables
+        *process.PUMcorrelation
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+            options.inputFiles
+        ),
+)
+
+import FWCore.PythonUtilities.LumiList as LumiList
+process.source.lumisToProcess = LumiList.LumiList(filename = 'run2015DExpress_Sep17.json').getVLuminosityBlockRange()
+
+
+process.TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string(options.outputFile)
+)
+
